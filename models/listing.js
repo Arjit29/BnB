b0452e035e04cef7921bd1c73df5413db77d7c00
Arjit29 <arjit.bhardwaj2004@gmail.com>
2024-07-19 11:39:29 +0530

    added owner ref

--- models/listing.js
+++ models/listing.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const Review = require("./review.js");
 
+
 const listingSchema = new Schema({
     title: {
         type: String,
@@ -33,7 +34,11 @@ const listingSchema = new Schema({
             type: Schema.Types.ObjectId,
             ref: "Review"
         }
-    ]
+    ],
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
 
     
 })
